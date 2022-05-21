db79332d370cd7b9d5233be8e8ffeef206364a89
GambleGeek <pansion33@gmail.com>
2022-05-22 02:47:45 +0600

    ff1

---
+++ app/src/main/java/com/ain_2_20_adis_kerimov/firebasetest/Constant.java
@@ -0,0 +1,7 @@
+package com.ain_2_20_adis_kerimov.firebasetest;
+
+public class Constant {
+    public static final String USER_NAME = "user_name";
+    public static final String USER_SEC_NAME = "user_sec_name";
+    public static final String USER_EMAIL = "user_email";
+}
---
+++ app/src/main/java/com/ain_2_20_adis_kerimov/firebasetest/User.java
@@ -0,0 +1,15 @@
+package com.ain_2_20_adis_kerimov.firebasetest;
+
+public class User {
+    public String id, name, secName, email;
+
+    public User() {
+    }
+
+    public User(String id, String name, String secName, String email) {
+        this.id = id;
+        this.name = name;
+        this.secName = secName;
+        this.email = email;
+    }
+}
